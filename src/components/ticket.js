2473a5058700719d15c52daecb4a4927ad38721e
jarenadams21 <jarenadams3@gmail.com>
2022-11-13 04:08:37 -0500

    my tickets page

--- src/components/ticket.js
+++ src/components/ticket.js
@@ -1,6 +1,6 @@
 export const Ticket = ({img, location, eventName}) => {
     return (
-        <a href="#" class="block rounded-lg p-4 shadow-sm shadow-indigo-100">
+        <a href="#" class="text-white m-6 block rounded-lg p-4 shadow-sm shadow-indigo-100">
   <img
     src={img}
     class="h-56 w-full rounded-md object-cover"
@@ -11,96 +11,39 @@ export const Ticket = ({img, location, eventName}) => {
       <div>
         <dt class="sr-only">Location</dt>
 
-        <dd class="text-sm text-gray-500">{location}</dd>
+        <dd class="text-sm text-red-500">{location}</dd>
       </div>
 
       <div>
         <dt class="sr-only">EventName</dt>
 
-        <dd class="font-medium">{eventName}</dd>
+        <dd class="font-small">{eventName}</dd>
       </div>
     </dl>
 
-    <div class="mt-6 flex items-center gap-8 text-xs">
-      <div class="sm:inline-flex sm:shrink-0 sm:items-center">
-        <svg
-          class="h-4 w-4 text-indigo-700"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
-          />
-        </svg>
-
-        <a
-          href="#"
-          class="mt-8 inline-flex items-center rounded border border-red-600 bg-red-600 px-8 py-3 text-white hover:bg-transparent focus:outline-none focus:ring active:text-pink-500"
-        >
-          <span class="text-sm font-medium"> Event Details </span>
-  
-          <svg
-            class="ml-3 h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M17 8l4 4m0 0l-4 4m4-4H3"
-            />
-          </svg>
-        </a>
-      </div>
-
-    
-
-      <div class="sm:inline-flex sm:shrink-0 sm:items-center">
-        <svg
-          class="h-4 w-4 text-indigo-700"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          />
-        </svg>
-
-        <a
-          href="#"
-          class="mt-8 inline-flex items-center rounded border border-red-600 bg-red-600 px-8 py-3 text-white hover:bg-transparent focus:outline-none focus:ring active:text-pink-500"
-        >
-          <span class="text-sm font-medium"> Show NFT </span>
-  
-          <svg
-            class="ml-3 h-5 w-5"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M17 8l4 4m0 0l-4 4m4-4H3"
-            />
-          </svg>
-        </a>
-      </div>
+    <div class="flex flex-wrap justify-between mt-4  items-center text-xs">
+    <a
+  class="group relative inline-block text-sm font-medium text-red-600 focus:outline-none focus:ring active:text-red-500"
+  href="/download"
+>
+  <span class="absolute inset-0 border border-current"></span>
+  <span
+    class="block border border-current bg-white px-12 py-3 transition-transform group-hover:-translate-x-1 group-hover:-translate-y-1"
+  >
+    Details
+  </span>
+</a>
+<a
+  class="group relative inline-block text-sm font-medium text-red-600 focus:outline-none focus:ring active:text-red-500"
+  href="/download"
+>
+  <span class="absolute inset-0 border border-current"></span>
+  <span
+    class="block border border-current bg-white px-12 py-3 transition-transform group-hover:-translate-x-1 group-hover:-translate-y-1"
+  >
+    NFT
+  </span>
+</a>
     </div>
   </div>
 </a>
--- src/pages/my-tickets.js
+++ src/pages/my-tickets.js
@@ -1,4 +1,6 @@
 import { Ticket } from '../components/ticket';
+import { Footer } from '../components/Footer';
+import Sidemenu from '../components/Sidemenu';
 const data = require("../utils/data.json")
 
 export const MyTickets = () => {
@@ -6,12 +8,34 @@ export const MyTickets = () => {
   const concertEvents = data.concerts;
     return (
 
-        <div>
+        
+        <div class="bg-gray-900">
+            <Sidemenu/>
+<header aria-label="Page Header">
+  <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+    <div class="flex items-center sm:justify-center sm:gap-4">
+      <div class="relative hidden sm:block">
+
+    <div class="mt-8">
+      <h1 class="flex justify-center text-2xl font-bold text-white sm:text-3xl">
+        Your Tickets
+      </h1>
+
+    </div>
+  </div>
+  </div>
+  </div>
+</header>
+
+       
+        <div className="grid grid-cols-2 lg:grid-cols-3">
         { concertEvents && concertEvents.map((event) => (
               <Ticket img={event.banner} location={event.location} eventName={event.name} />
                      
           ))}
-          
+
+        </div>
+        <Footer/>
         </div>
     )
 }
