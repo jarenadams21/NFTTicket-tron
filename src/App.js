d30fd1f959fcb6d9873d5956091c2d6f8578eb6d
Nishay Madhani <nshmadhani@gmail.com>
2022-11-12 19:54:53 -0500

    router, folder scheme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Landing } from './landing';
+import { Landing } from './pages/landing';
 import "tailwindcss/tailwind.css";
 
 function App() {
